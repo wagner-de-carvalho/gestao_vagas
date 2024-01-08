1b2a242321e19bb02afa63426bf6e73ea9a2e0e8
wagner <wagncarv@outlook.com>
2024-01-08 01:17:40 -0300

    Company Controller

---
+++ src/main/java/br/com/acme/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,29 @@
+package br.com.acme.gestao_vagas.modules.company.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.acme.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.acme.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    private CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+        try {
+            var result = this.createCompanyUseCase.execute(companyEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
