dc4548015fcfd871c98ba38debbe9f039c13d821
wagner <wagncarv@outlook.com>
2024-01-08 00:37:57 -0300

    Entidade Job

--- src/main/java/br/com/acme/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/acme/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,5 +1,32 @@
 package br.com.acme.gestao_vagas.modules.company.entities;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "job")
 public class JobEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne
+    private CompanyEntity companyEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdA;
 
 }
