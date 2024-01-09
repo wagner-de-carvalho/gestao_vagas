c68b7e347eac6133ca340936091ccb9d6a6c424d
wagner <wagncarv@outlook.com>
2024-01-08 23:37:21 -0300

    Recuperando JWT

--- src/main/java/br/com/acme/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/acme/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.acme.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +41,7 @@ public class AuthCompanyUseCase {
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         var token = JWT.create().withIssuer("javagas")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
                 .sign(algorithm);
 
