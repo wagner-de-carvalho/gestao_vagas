22998a2e8cbb31f786b8f64affd1aaf6843e9c17
wagner <wagncarv@outlook.com>
2024-01-16 14:47:12 -0300

    Criando entidade de Apply Job

--- src/main/java/br/com/acme/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/acme/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import br.com.acme.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.acme.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.acme.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.acme.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.acme.gestao_vagas.modules.company.repositories.JobRepository;
 import lombok.AllArgsConstructor;
 
@@ -15,6 +16,7 @@ import lombok.AllArgsConstructor;
 public class ApplyJobCandidateUseCase {
     private CandidateRepository candidateRepository;
     private JobRepository jobRepository;
+    private ApplyJobRepository applyJobRepository;
 
     public String execute(UUID candidateId, UUID jobId) {
         this.candidateRepository.findById(candidateId)
